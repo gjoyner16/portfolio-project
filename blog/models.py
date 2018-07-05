287dbac9609334b4bfd5b3d49d4688a965bc85bf
Unknown <griffintjoyner@gmail.com>
2018-07-04 20:06:27 -0400

    Created blog page and detail blog pages

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,12 @@ class Blog(models.Model):
     pub_date = models.DateField(default=timezone.now)
     image = models.ImageField(upload_to='images/')
     body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
